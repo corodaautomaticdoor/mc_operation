07647cee36538d92e1d2501ca3bdf38310a1b308
Gary Alberto Chavez Jimenez <gchavezj@everis.com>
2020-03-06 16:41:03 -0500

    'creacion_microservicio_operation'

---
+++ src/main/java/com/coroda/mc_operation/McOperationApplication.java
@@ -0,0 +1,13 @@
+package com.coroda.mc_operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coroda/mc_operation/McOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coroda.mc_operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
