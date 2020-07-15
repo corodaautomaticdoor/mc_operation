2ec1d2aec7ef71f1b0a902e1b0ecbf0d7ef1abad
Gary <garychavezj@gmail.com>
2020-07-14 20:39:58 -0500

    feat : implementa logica de la capa dto

--- src/main/java/com/coroda/dto/request/DetailOperationRequest.java
+++ src/main/java/com/coroda/dto/request/DetailOperationRequest.java
@@ -1,5 +1,28 @@
 package com.coroda.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+@ApiModel("Model DetailOperation")
 public class DetailOperationRequest {
+
+    @ApiModelProperty(value = "id")
+    private Long id;
+
+    @ApiModelProperty(value = "detailOperationId")
+    private Long detailOperationId;
+
+    @ApiModelProperty(value = "description")
+    private String description;
+
+    @ApiModelProperty(value = "quantity", required = true)
+    private BigDecimal quantity;
+
+    @ApiModelProperty(value = "priceUnit", required = true)
+    private BigDecimal priceUnit;
+
 
 }
--- src/main/java/com/coroda/dto/request/OperationRequest.java
+++ src/main/java/com/coroda/dto/request/OperationRequest.java
@@ -1,5 +1,35 @@
 package com.coroda.dto.request;
 
+import com.coroda.entity.TypeOperation;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel("Model Operation")
 public class OperationRequest {
 
+    @ApiModelProperty(value = "operationId")
+    private Long operationId;
+
+    @ApiModelProperty(value = "typeOperation", required = true)
+    private TypeOperation typeOperation;
+
+    @ApiModelProperty(value = "date", required = true)
+    private Date date;
+
+    @ApiModelProperty(value = "client", required = true)
+    private String client;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<DetailOperationRequest> detailOperation;
 }
