e9b460b1adce5f0e4fe17048faccab17d0c32dd9
Gary <garychavezj@gmail.com>
2020-07-14 20:45:37 -0500

    fix : mejora logica de la capa dto

---
+++ src/main/java/com/coroda/dto/response/TypeOperationResponse.java
@@ -0,0 +1,12 @@
+package com.coroda.dto.response;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class TypeOperationResponse {
+    int update;
+}
